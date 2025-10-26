ab6e9ef42d65b8fc801692fbbb36231de1c9a150
Adriana Chiquito <adrianacp108@gmail.com>
2025-10-26 15:31:22 +0100

    added apuntes

--- proyecto-gsap/src/pages/_app.js
+++ proyecto-gsap/src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import '../styles/style.css';  // Asegúrate de que la ruta sea correcta
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
